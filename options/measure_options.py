c18596bac30f07b23cc6869fa61c78f293230de1
maimand <42661871+maimand@users.noreply.github.com>
2022-09-26 23:02:20 +0700

    Update measure_options.py
    Update sample file name

--- options/measure_options.py
+++ options/measure_options.py
@@ -2,4 +2,4 @@ from .train_options import TrainOptions
 class MeasureOptions(TrainOptions):
     def initialize(self):
         TrainOptions.initialize(self)
-        self.parser.add_argument('--image_name', type=str, default='./data/inputs/11.jpg', help='Name of input image')
+        self.parser.add_argument('--image_name', type=str, default='204.jpg', help='Name of input image')
